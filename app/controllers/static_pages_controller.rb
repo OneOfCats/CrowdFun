b4c6450bca5702b3525684059fb4c8858f94e11e
OneOfCats <krableshtraus@gmail.com>
2017-04-05 01:38:52 +0300

    sql query for home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @projects = Project.where(published: true)
-    @projects_top_admins = @projects.joins(:votes).where status: Vote.statuses[:liked]
-    @project_main = @projects_top_admins.order("RANDOM()").first
+    @projects_top_admins = Project.find_by_sql ("SELECT projects.*, (100 - COALESCE((SELECT COUNT(votes.id) FROM votes WHERE votes.status = 1 AND votes.project_id = projects.id AND votes.group = 1) * 100 / NULLIF((SELECT COUNT(votes.id) FROM votes WHERE votes.project_id = projects.id AND votes.group = 1), 0), 0)) AS admins_rating FROM projects WHERE projects.published = TRUE ORDER BY admins_rating LIMIT 10;") 
+    @project_main = @projects_top_admins[rand(@projects_top_admins.count)]
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -31,8 +31,7 @@ Rails.application.routes.draw do
   get 'search' => 'static_pages#search', as: :search
   get 'about' => 'static_pages#about', as: :about
 
-  root 'application#index'
-  # root 'static_pages#home'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
